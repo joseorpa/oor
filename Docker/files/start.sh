bf589ad7cf54f6a848351b0b5613aa38d3cb9df5
Jose Ortiz <jose.orpa@gmail.com>
2017-11-19 18:23:13 +0100

    Parametrització

--- Docker/files/start.sh
+++ Docker/files/start.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-sed -i s/IPV4EID/$1/g /etc/oor.conf
-sed -i s/IPV6EID/$2/g /etc/oor.conf
-oor -f /etc/oor.conf
+cat <<EOT >> /etc/ansible/hosts
+
+[targets]
+
+localhost              ansible_connection=local
+EOT
+
+sed -i s/DEBUG/$1/g /tmp/oor.yaml
+sed -i s/OPMODE/$2/g /tmp/oor.yaml
+sed -i s/IPMAPRESOLVER/$3/g /tmp/oor.yaml
+sed -i s/IPMAPSERVER/$4/g /tmp/oor.yaml
+sed -i s/KEYMAPSERVER/$5/g /tmp/oor.yaml
+sed -i s/IPPROXYETRV4/$6/g /tmp/oor.yaml
+sed -i s/IPPROXYETRV6/$7/g /tmp/oor.yaml
+sed -i s/IPV4EIDPREFFIX/$8/g /tmp/oor.yaml
+sed -i s/IPV6EIDPREFFIX/$9/g /tmp/oor.yaml
+#ansible-playbook oor.yaml
+#oor -f /etc/oor.conf
